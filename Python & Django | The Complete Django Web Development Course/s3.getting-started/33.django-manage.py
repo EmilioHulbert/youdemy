27e5457bb463c3c1ccc557bdce0e487c7ad80732
lilplucky <emiliohulbert2017@gmail.com>
2025-02-26 09:02:26 +0300

    My learning As Of  Wed Feb 26 09:02:26 AM EAT 2025

---
+++ Python & Django | The Complete Django Web Development Course/s3.getting-started/33.django-manage.py
@@ -0,0 +1,7 @@
+python manage.py --help
+python manage.py runserver
+ORM object relation mapper
+migration is py code we write to interact with sql db
+python manage.py migrate
+django-admin only used when creating a new project 
+
