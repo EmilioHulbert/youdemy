c921edff8c1aa2a9bdd0c5edaea3c84ec7a765e0
lilplucky <emiliohulbert2017@gmail.com>
2025-03-20 16:52:35 +0300

    My learning As Of  Thu Mar 20 04:52:35 PM EAT 2025

---
+++ php-essentials/resources/phptutorials/datatypes.php
@@ -0,0 +1,22 @@
+<?php 
+// string
+$a = "Meta brains";
+echo $a. "<br>";
+
+//Number
+$b = 73479.89;
+echo $b. "<br>";
+
+//Boolean
+$c = false;
+echo $c. "<br>";
+
+//Array
+$d=array("PHP", "MYSQL", "JAVA");
+echo $d[2]. "<br>";
+
+$e=null;
+echo $b. "<br>";
+
+var_dump($e);
+?>
