14d3c64328b317ae562f85d818d2f11e15b553ca
lilplucky <emiliohulbert2017@gmail.com>
2025-12-04 15:26:27 +0300

    My learning As Of  Thu Dec  4 03:26:27 PM EAT 2025

--- The Complete Vue.JS Course for Beginners: Zero to Mastery/vue.js/post/src/components/loginform.vue
+++ The Complete Vue.JS Course for Beginners: Zero to Mastery/vue.js/post/src/components/loginform.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Simple Form</h1>
-<form action="">
+<!-- <form action=""> -->
     <label for="">Username</label>&nbsp;
     <input type="text" name="" id="" placeholder="username" v-model="form.username"/>
     <br/>
@@ -30,8 +30,15 @@
 
 
     <button v-on:click="login">Sign Up</button>
-</form>
-<p>{{ form }}</p>
+    <!-- <button type="button" @click="login">Sign Up</button> -->
+
+<!-- </form> -->
+<!-- <p>{{ form }}</p> -->
+ <ul>
+    <li v-for="item in fun" v-bind:key="item">
+        {{ item }} Not Submitted
+    </li>
+ </ul>
 </template>
 <script>
     export default {
@@ -42,14 +49,27 @@
                 username: "",
                 password: "",
                 hobbies: [],
+                gender: [],
             },
+            fun: [],
         };
     },
     
     methods:{
         login(){
+            this.fun = [];
             // console.log("login data", this.form);
-            console.table("login data", this.form);
+            for(const item in this.form){
+                if(this.form[item] === "" || this.form[item].length === 0){
+                    this.fun.push(item);
+                }
+            }
+            // console.table("login data", this.form);
+            if(this.fun.length ===0 ){
+                alert("Information has been submitted")
+            }
+            // console.log(this.form, this.fun);
+            console.table(this.form, this.fun);
         },
     },
     };
