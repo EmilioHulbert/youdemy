8d1e1e29aa834cd952bc859532708519f230ee2d
lilplucky <emiliohulbert2017@gmail.com>
2025-12-08 13:31:02 +0300

    My learning As Of  Mon Dec  8 01:31:02 PM EAT 2025

--- The Complete Vue.JS Course for Beginners: Zero to Mastery/vue.js/post/src/components/beforecreate.vue
+++ The Complete Vue.JS Course for Beginners: Zero to Mastery/vue.js/post/src/components/beforecreate.vue
@@ -1,18 +1,44 @@
 <template>
-    <h1>beforeCreate and created</h1>
+    <!-- <h1>beforeCreate and created</h1> -->
+    <!-- <h1>beforeMount and Mounted</h1> -->
+     <!-- <h1 ref="adding">count:{{counter}}</h1> -->
+<!-- <button v-on:click="counter = counter + 1"> Update the number</button> -->
+<h1>beforeunmount and unmounted</h1>
+<button v-on:click="show = !show">Togglebar</button>
+<Lifecyclechild v-if="show" />
  </template>
  <script>
+ import Lifecyclechild from "./lifecyclechild.vue";
     export default {
    name: "beforecreate-com",
    data(){
-    console.error("created")
+    // console.error("created");
+    return{
+        counter: 0,
+        show:true,
+    };
    },
-   beforeCreate(){
-        alert("Page loading...");
-    },
-    created(){
-        alert("Page Loaded");
-    },
+//    beforeCreate(){
+//         alert("Page loading...");
+//     },
+//     created(){
+//         alert("Page Loaded");
+//     },
+// beforeMount(){
+//     console.log("This is a beforeMount Method", this.$el);
+// },
+// mounted(){
+//     console.log("This is a mounted Method", this.$el);
+// },
+// beforeUpdate(){
+// // console.log("beforeUpdate", this.counter);
+// console.log("beforeUpdate", this.$refs["adding"].textContent);
+// },
+// updated(){
+//     console.log("updated", this.$refs["adding"].textContent);
+// },
+
+components:{Lifecyclechild,},
     };
    
  </script>
